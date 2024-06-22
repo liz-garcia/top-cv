0767de9545967c159510b4f5e131ee8b1e0008aa
liz-garcia <lizgarciamarin@gmail.com>
2024-06-22 15:52:39 +0200

    Fixed server/index.js file to serve client files in production.

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,40 @@
-import app from "./app.js";
 import dotenv from "dotenv";
+import express from "express";
+import app from "./app.js";
+import URL from "url";
 
-const environment = process.env.NODE_ENV || "production";
+const environment = process.env.NODE_ENV;
 dotenv.config({ path: `.env.${environment}` });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+
+if (port == null) {
+  // If this fails, make sure you have created the `.env` files in the right place with the PORT set
+  console.error(
+    new Error("Cannot find a PORT number, did you create the .env files?")
+  );
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+/****** Host our client code for Heroku *****/
+/**
+ * We only want to host our client code when in production mode as we then want to use the production build that is built in the dist folder.
+ * When not in production, don't host the files, but the development version of the app can connect to the backend itself.
+ */
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(new URL("../../client/dist", import.meta.url).pathname)
+  );
+  // Redirect * requests to give the client data
+  app.get("*", (req, res) =>
+    res.sendFile(
+      new URL("../../client/dist/index.html", import.meta.url).pathname
+    )
+  );
+}
 
 app.listen(port, (err) => {
   if (err) {
