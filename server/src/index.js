32d52ae543fd7145f551e16304d9cd8e2ce09f55
liz-garcia <lizgarciamarin@gmail.com>
2024-06-22 15:34:03 +0200

    Configure for Heroku deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,21 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-const environment = process.env.NODE_ENV;
+
+const environment = process.env.NODE_ENV || "production";
 dotenv.config({ path: `.env.${environment}` });
 
-// * The environment should set the port
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(
-    `Server running in ${environment} mode on port ${port} at http://localhost:${port}/`
-  );
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Failed to start server:", err);
+  } else {
+    console.log(
+      `Server running in ${environment} mode on port ${port} at http://localhost:${port}/`
+    );
+  }
 });
