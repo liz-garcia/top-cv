ab72bf6bac44f419253b2222799e81891886b803
liz-garcia <lizgarciamarin@gmail.com>
2024-06-26 20:26:10 +0200

    Modified some comments.

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,10 +8,9 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Default is '' because on our heroku servers
+  // Default is "" because on our heroku servers
   // we want to have it default to our current URL
   define: {
-    // "import.meta.env.VITE_BASE_SERVER_URL": JSON.stringify(""),
     "import.meta.env.VITE_BASE_SERVER_URL": JSON.stringify(
       process.env.UI_BASE_URL || ""
     ),
